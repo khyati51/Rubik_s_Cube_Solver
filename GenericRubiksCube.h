d473d0cf5464f7adc889f92b6bb03c8d7279ee50
khyati51 <khyatigupta14@kgpian.iitkgp.ac.in>
2024-06-27 22:02:51 +0530

    Added GenericRubikCube Model

--- GenericRubiksCube.h
+++ GenericRubiksCube.h
@@ -4,11 +4,39 @@
 
 #ifndef GENERICRUBIKSCUBE_H
 #define GENERICRUBIKSCUBE_H
-
+#include<bits/stdc++.h>
+using namespace std;
 
 
 class GenericRubiksCube {
+public:
+    enum class FACE{
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT,
+        FRONT,
+        BACK
+    };
 
+    enum class COLOR{
+        RED,
+        BLUE,
+        GREEN,
+        YELLOW,
+        WHITE,
+        ORANGE
+    };
+
+    enum class MOVE {
+        L, LPRIME, L2,
+        R, RPRIME, R2,
+        F, FPRIME, F2,
+        B, BPRIME, B2,
+        U, URPIME, U2,
+        D, DPRIME, D2
+
+    };
 };
 
 
