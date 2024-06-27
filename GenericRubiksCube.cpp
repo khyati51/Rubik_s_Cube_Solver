faf38fa72c600e61e24df4b01dab8579c363a4f0
khyati51 <khyatigupta14@kgpian.iitkgp.ac.in>
2024-06-27 14:25:14 +0530

    Added GenericRubikCube Model

---
+++ GenericRubiksCube.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Khyati on 6/27/24.
+//
+
+#include "GenericRubiksCube.h"
---
+++ GenericRubiksCube.h
@@ -0,0 +1,16 @@
+//
+// Created by Khyati on 6/27/24.
+//
+
+#ifndef GENERICRUBIKSCUBE_H
+#define GENERICRUBIKSCUBE_H
+
+
+
+class GenericRubiksCube {
+
+};
+
+
+
+#endif //GENERICRUBIKSCUBE_H
